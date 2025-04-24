51cd46799a9efc82cb7b36321ae8c45b22657a6b
cesaruacj <al234525@alumnos.uacj.mx>
2025-04-23 19:45:52 -0600

    kk

---
+++ scripts/deployFlashLoan.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  console.log("deploying...");
+  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
+  const flashLoan = await FlashLoan.deploy(
+    "0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A"
+  );
+
+  await flashLoan.deployed();
+
+  console.log("Flash loan contract deployed: ", flashLoan.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
